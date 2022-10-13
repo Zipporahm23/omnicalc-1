6446437b358792bb41b76a97a5143a25ff894385
Zipporah <shemaiahm1@gmail.com>
2022-10-13 19:11:15 +0000

    min and max

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,19 @@ def calculate_square
   # params ={"value"=>"322"}
   @num = params.fetch("value").to_f
   @square_of_num = @num ** 2
-  render({ :template => "calculation_templates/square_results.html.erb"})
+  render({ :template => "calculation_templates/square_results.html.erb"}) 
+end
+
+def blank_random_form
+  render({ :template => "calculation_templates/rand_form.html.erb"})
+end
+
+def calculate_random
+  # params = {"user_min="322}
+  @lower = params.fetch("user_min").to_f
+
+  @upper = params.fetch("user_max").to_f
+  render({ :template => "calculation_templates/rand_results.html.erb"})
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # Calculate Square root
 get("/square/new", { :controller => "application", :action => "blank_square_form" })
-get("/square/results", { :controller => "application", :action => "calculate_square"})
+
+get("/square/results", { :controller => "application", :action => "calculate_square" })
+
+# random with form
+get("/random/results", { :controller => "application", :action => "calculate_random" })
+
+get("/random/new", { :controller => "application", :action => "blank_random_form" })
 end
